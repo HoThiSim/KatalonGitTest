af29570fd8acae71d6fcf4db85e01ae4d6e44790
PNV <PNV@SGH649TX82.kms.com.vn>
2021-01-20 11:09:27 +0700

    Added from Demofirst Project

---
+++ Test Suites/KatalonGitTest.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatalonGitTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cb640ad7-586d-47be-b5fb-f8614b3124a2</testSuiteGuid>
+   <testCaseLink>
+      <guid>afbad0cc-4d9e-42bc-96cc-d837cb2fa7d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1BySim</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>05436acf-3cf8-4a7d-9972-bb0c7449db7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2BySim</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
